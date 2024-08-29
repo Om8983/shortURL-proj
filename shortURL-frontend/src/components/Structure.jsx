f7e8b2616b981d9de98b47c1d9d59ef05dd30dd1
om wadhi <lostboybegining@gmail.com>
2024-08-29 18:33:24 +0530

    axios err fixed

--- shortURL-frontend/src/components/Structure.jsx
+++ shortURL-frontend/src/components/Structure.jsx
@@ -1,43 +1,36 @@
 import { useState } from "react"
 import axios from "axios"
-export function Structure(){
+export function Structure() {
     const [url, setUrl] = useState('')
     const [shortUrlID, setShortURL] = useState('')
 
-    async function  handleClick() {
-        
-        try{
+    async function handleClick() {
 
+        try {
             // axios code 
-            // let res = await axios.post(`http://localhost:3000/urlShortner/shortUrl`,  url   )
-            
-            //fetch code
-            let res = await fetch(`http://localhost:3000/urlShortner/shortUrl`, {
-                method : 'POST',
-                body : JSON.stringify({
-                    url : url
-                }),
-                headers : {
-                    'Content-Type' : 'application/json'
-                }
-            })
-            
-        }catch(e){
-            console.error("error occured while fetching url",e);
-
+            let res = await axios.post(`http://localhost:3000/urlShortner/shortUrl`, { url })
+            if (res.status == 200) {
+                let { shortUrl } = res.data
+                setShortURL(shortUrl)
+            }
+        } catch (e) {
+            console.error("error occured while fetching url", e);
         }
-        
-       
-    } 
+    }
 
     return (
         <>
             <input type="text" placeholder="Enter URL " onChange={(e) => setUrl(e.target.value)} /> <br /><br />
             <button onClick={handleClick}> Get Shortened URL </button> <br /><br />
-            {/* <div>
-                {url}
-            </div> */}
-            
+
+            {/* // conditional rendering */}
+            {shortUrlID !== "" ? (
+                <div>
+                    Short URL: http://localhost:3000/{shortUrlID} <br /><br />
+                    Original URL : {url}
+                </div>
+            ) : ""}
+
         </>
     )
 }
